88b2c721c87f874808d13294ef42e726e00b49ca
ivan <pagdayawonjohnivan@gmail.com>
2021-10-13 17:52:13 +0800

    test

--- server.js
+++ server.js
@@ -25,59 +25,6 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/movies', require('./routes/api/movies'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-
-app.get('/', (request, response) => {
-    const filePath = path.resolve(__dirname, 'client', 'build', 'index.html');
-    fs.readFile(filePath, 'utf-8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        data = data.replace(/\$OG_TITLE/g, 'GoMovie | Watch Movie');
-        data = data.replace(/\$OG_DESCRIPTION/g, "At GoMovie, we want to entertain the world. Whatever your taste, and no matter where you live, we give you access to best-in-class TV shows, movies and documentaries.");
-        result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
-        response.send(result);
-    });
-});
-
-
-app.get('/home', (request, response) => {
-    const filePath = path.resolve(__dirname, 'client', 'build', 'index.html');
-    fs.readFile(filePath, 'utf-8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        data = data.replace(/\$OG_TITLE/g, 'GoMovie: Watch Movie Online · GoMovie');
-        data = data.replace(/\$OG_DESCRIPTION/g, "Watch GoMovie movies & TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more.");
-        result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
-        response.send(result);
-    });
-});
-app.get('/login', (request, response) => {
-    const filePath = path.resolve(__dirname, 'client', 'build', 'index.html');
-    fs.readFile(filePath, 'utf-8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        data = data.replace(/\$OG_TITLE/g, 'Login | GoMovie');
-        data = data.replace(/\$OG_DESCRIPTION/g, "Watch GoMovie movies & TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more, soo Login now.");
-        result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
-        response.send(result);
-    });
-});
-
-app.get('/signup', (request, response) => {
-    const filePath = path.resolve(__dirname, 'client', 'build', 'index.html');
-    fs.readFile(filePath, 'utf-8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        data = data.replace(/\$OG_TITLE/g, 'Signup | GoMovie');
-        data = data.replace(/\$OG_DESCRIPTION/g, "Watch GoMovie movies & TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more, soo Signup now.");
-        result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
-        response.send(result);
-    });
-});
-
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     //set static folder
